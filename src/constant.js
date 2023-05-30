68a241752c9fcbd6a7b1b9d48e47c0ef2ff03b84
MarufaKompass <marufa@hellokompass.com>
2023-05-30 17:44:14 +0600

    setup router

---
+++ src/constant.js
@@ -0,0 +1,53 @@
+export const routes = [
+    {
+      name: "Home",
+      link: "/",
+    },
+   
+    {
+      name: "My Account",
+      link: "/my-account",
+  
+      subRoutes: [
+        {
+          name: "My Meal Plan",
+          link: "/meal-plan",
+        },
+       
+        {
+          name: "My subscription",
+          link: "subscription",
+        },
+       
+      ],
+    },
+    {
+      name: "Blog",
+      link: "/blog",
+    },
+   
+    {
+      name: "About Us",
+      link: "/about",
+    },
+  
+    {
+      name: "Contact Us",
+      link: "/contact",
+    },
+    {
+      name: "Reviews",
+      link: "/reviews",
+    },
+    {
+      name: "Contact Us",
+      link: "/contact",
+    },
+    {
+      name: "Terms & Policies",
+      link: "/policy",
+    },
+   
+    
+  ];
+  
